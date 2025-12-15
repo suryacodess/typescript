1350087bbaee2ce959df29f4468c003fc8b77601
suryacodess <helloosuryaprakash@gmail.com>
2025-12-15 17:01:19 +0530

    enum types

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// tuples
+// tuples - With tuples we can define the type of the variable
 let str = "Surya prakash";
 let num = 1212;
 let arr = 2323; //error because, arr supposed to store array not number
 let arr1 = [1, "2", 3, 4];
 let userId = Number("12");
 let userVisited = true;
+// enums - With enums we can create variables or properties and easily fetch them all at once, also know as enumerations.
+var errorsMessage;
+(function (errorsMessage) {
+    errorsMessage["notFoundError"] = "401 Not found error";
+    errorsMessage["successfull"] = "200 Successfull";
+    errorsMessage["failed"] = "402 Failed";
+    errorsMessage["serverError"] = "500 Internal Server";
+})(errorsMessage || (errorsMessage = {}));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,7 +1,15 @@
-// tuples
+// tuples - With tuples we can define the type of the variable
 let str: string = "Surya prakash";
 let num: number = 1212;
 let arr: number = 2323; //error because, arr supposed to store array not number
 let arr1: [number, string, number, number] = [1, "2", 3, 4];
 let userId: number = Number("12");
 let userVisited: boolean = true;
+
+// enums - With enums we can create variables or properties and easily fetch them all at once, also know as enumerations.
+enum errorsMessage {
+  notFoundError = "401 Not found error",
+  successfull = "200 Successfull",
+  failed = "402 Failed",
+  serverError = "500 Internal Server",
+}
