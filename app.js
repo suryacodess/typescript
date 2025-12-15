146f5cb141f772f747d3c5373603f55d6c50fe0c
suryacodess <helloosuryaprakash@gmail.com>
2025-12-15 16:11:43 +0530

    tuples types

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-var a = 20;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// tuples
+let str = "Surya prakash";
+let num = 1212;
+let arr = 2323; //error because, arr supposed to store array not number
+let arr1 = [1, "2", 3, 4];
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
-interface users {
-  name: string;
-  id: number;
-}
-
-var a = 20;
+// tuples
+let str: string = "Surya prakash";
+let num: number = 1212;
+let arr: [] = 2323; //error because, arr supposed to store array not number
+let arr1: [number, string, number, number] = [1, "2", 3, 4];
