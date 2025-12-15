f1c55e643b8e0c228b902e7c8b4d1856c7045447
suryacodess <helloosuryaprakash@gmail.com>
2025-12-15 16:14:40 +0530

    more examples of tuples

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ let str = "Surya prakash";
 let num = 1212;
 let arr = 2323; //error because, arr supposed to store array not number
 let arr1 = [1, "2", 3, 4];
+let userId = Number("12");
+let userVisited = true;
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 // tuples
 let str: string = "Surya prakash";
 let num: number = 1212;
-let arr: [] = 2323; //error because, arr supposed to store array not number
+let arr: number = 2323; //error because, arr supposed to store array not number
 let arr1: [number, string, number, number] = [1, "2", 3, 4];
+let userId: number = Number("12");
+let userVisited: boolean = true;
