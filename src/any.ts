33219995e15f782d2de1c6944c52da8695f12ce4
suryacodess <helloosuryaprakash@gmail.com>
2025-12-22 18:10:20 +0530

    02.07 Any data type

---
+++ src/any.ts
@@ -0,0 +1,10 @@
+// Any type in ts
+// to define any type we use :any
+// any type used when we dont know what kind of the value will it be
+
+let a: any = { id: 0, name: "Surya" };
+let nun: any = 200000000;
+nun = "Surya";
+nun = [1, 1, 2, 3, 3, 4];
+// nun = 2
+console.log(nun.join(","))
