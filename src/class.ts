48c053bd39dc8445f8fd44058f7ad2d0c374c5b2
suryacodess <helloosuryaprakash@gmail.com>
2025-12-23 11:42:01 +0530

    09 Classes in ts

---
+++ src/class.ts
@@ -0,0 +1,47 @@
+// Class in Ts
+// Classes are blueprint for the objects
+class Product {
+  id: string | number;
+  productName: string;
+  productPrice: string | number;
+  inCart: boolean;
+  constructor(
+    id: string | number,
+    productName: string,
+    productPrice: string | number,
+    inCart: boolean
+  ) {
+    this.id = id;
+    this.inCart = inCart;
+    (this.productName = productName), (this.productPrice = productPrice);
+  }
+
+  productInCart(): string {
+    if (this.inCart) {
+      return "Product is in cart";
+    } else {
+      return "Product is not in cart";
+    }
+  }
+}
+
+interface product {
+  id: string | number;
+  productName: string;
+  productPrice: string | number;
+  inCart: boolean;
+}
+const productDetails: product = {
+  id: 0,
+  productName: "Iphone 17",
+  productPrice: "$1800",
+  inCart: false,
+};
+const product1 = new Product(
+  productDetails.id,
+  productDetails.productName,
+  productDetails.productPrice,
+  productDetails.inCart
+);
+console.log(product1);
+console.log(product1.productInCart());
