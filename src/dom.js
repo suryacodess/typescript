9512ca1f49a5b3518c58cba1b1e5080b58098a3f
suryacodess <helloosuryaprakash@gmail.com>
2025-12-23 11:16:12 +0530

    08 Getting Dom Elements in Ts

---
+++ src/dom.js
@@ -0,0 +1,6 @@
+var h1 = document.querySelector("h1");
+console.log(h1.innerText);
+var anchor = document.querySelector("a");
+console.log(anchor.href);
+var h1Elem = document.querySelector("h1");
+console.log(h1Elem === null || h1Elem === void 0 ? void 0 : h1Elem.innerText);
---
+++ src/dom.ts
@@ -0,0 +1,7 @@
+const h1 = document.querySelector("h1") as HTMLHeadingElement;
+console.log(h1.innerText);
+const anchor = document.querySelector("a")!;
+console.log(anchor.href);
+
+const h1Elem = document.querySelector("h1");
+console.log(h1Elem?.innerText);
